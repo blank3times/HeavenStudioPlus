2e94ccee9ca097a1768872474388ace60fd7c349
blank3times <105398129+blank3times@users.noreply.github.com>
2024-08-03 01:01:47 -0700

    The old cocoa bar work is back

---
+++ Assets/Scripts/Games/CocoaBar/CocoaBar.cs
@@ -0,0 +1,149 @@
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using HeavenStudio.Util;
+using HeavenStudio.InputSystem;
+
+using Jukebox;
+
+namespace HeavenStudio.Games.Loaders
+{
+    using static Minigames;
+    
+    public static class PcoCocoaBarLoader
+    {
+        public static Minigame AddGame(EventCaller eventCaller)
+        {
+            return new Minigame("cocoaBar", "Cocoa Bar [Tentative]", "211571", false, false, new List<GameAction>()
+            {
+                new GameAction("pancake", "Pancake")
+                {
+                    function = delegate { CocoaBar.instance.Pancake(eventCaller.currentEntity.beat); },
+
+                    defaultLength = 4f
+                },
+                new GameAction("cocoa", "Hot Cocoa")
+                {
+                    function = delegate { CocoaBar.instance.Cocoa(eventCaller.currentEntity.beat); },
+
+                    defaultLength = 3.5f
+                }
+            }
+            );
+        }
+    }
+}
+
+namespace HeavenStudio.Games
+{
+    /// This class handles the minigame logic.
+    /// Minigame inherits directly from MonoBehaviour, and adds Heaven Studio specific methods to override.
+    public class CocoaBar : Minigame
+    {
+        public static CocoaBar instance;
+
+        const int IAAltDownCat = IAMAXCAT;
+        const int IAAltUpCat = IAMAXCAT + 1;
+
+        protected static bool IA_PadAltPress(out double dt)
+        {
+            return PlayerInput.GetPadDown(InputController.ActionsPad.South, out dt);
+        }
+        protected static bool IA_BatonAltPress(out double dt)
+        {
+            return PlayerInput.GetSqueezeDown(out dt);
+        }
+        
+        protected static bool IA_PadAltRelease(out double dt)
+        {
+            return PlayerInput.GetPadUp(InputController.ActionsPad.South, out dt);
+        }
+        protected static bool IA_BatonAltRelease(out double dt)
+        {
+            return PlayerInput.GetSqueezeUp(out dt);
+        }
+
+        public static PlayerInput.InputAction InputAction_AltPress =
+            new("PcoCocoaBarAltPress", new int[] { IAAltDownCat, IAAltDownCat, IAAltDownCat },
+            IA_PadAltPress, IA_TouchBasicPress, IA_BatonAltPress);
+        public static PlayerInput.InputAction InputAction_AltRelease =
+            new("PcoCocoaBarAltRelease", new int[] { IAAltUpCat, IAFlickCat, IAAltUpCat },
+            IA_PadAltRelease, IA_TouchFlick, IA_BatonAltRelease);
+        public static PlayerInput.InputAction InputAction_TouchRelease =
+            new("PcoCocoaBarTouchRelease", new int[] { IAEmptyCat, IAReleaseCat, IAEmptyCat },
+            IA_Empty, IA_TouchBasicRelease, IA_Empty);
+
+
+        void Awake()
+        {
+            instance = this;
+        }
+
+        public void Pancake(double beat)
+        {
+            ScheduleInput(beat, 2f, InputAction_BasicPress, PlateSuccess, ServeMiss, Nothing);
+            ScheduleInput(beat, 2.75f, InputAction_BasicPress, PlateSuccess, ServeMiss, Nothing);
+            ScheduleInput(beat, 3.5f, InputAction_BasicPress, PlateSuccess, ServeMiss, Nothing);            
+            MultiSound.Play(new MultiSound.Sound[] {
+                new MultiSound.Sound("cocoaBar/ding_temp", beat),
+                new MultiSound.Sound("cocoaBar/customer_temp", beat + 1f),
+                new MultiSound.Sound("cocoaBar/customer_temp", beat + 1.5f)
+            });
+        }
+
+        public void Cocoa(double beat)
+        {
+            ScheduleInput(beat, 1.5f, InputAction_AltPress, FillSuccess, ServeMiss, Nothing);
+            ScheduleInput(beat, 2.5f, InputAction_AltRelease, CupSuccess, ServeMiss, Nothing);
+            MultiSound.Play(new MultiSound.Sound[] {
+                new MultiSound.Sound("cocoaBar/ding_temp", beat),
+                new MultiSound.Sound("cocoaBar/customer_temp", beat + 0.5f),
+                new MultiSound.Sound("cocoaBar/customer_temp", beat + 1f)
+            });
+        }
+
+        public void PlateSuccess(PlayerActionEvent caller, float state)
+        {
+            if (state >= 1f || state <= -1f)
+            {
+                SoundByte.PlayOneShotGame("cocoaBar/whiff_temp");
+                return;
+            }
+            SoundByte.PlayOneShotGame("cocoaBar/whoosh_temp");
+        }
+
+        public void FillSuccess(PlayerActionEvent caller, float state)
+        {
+            if (state >= 1f || state <= -1f)
+            {
+                SoundByte.PlayOneShotGame("cocoaBar/whiff_temp");
+                return;
+            }
+            SoundByte.PlayOneShotGame("cocoaBar/marshmallows_temp");
+        }
+
+        public void CupSuccess(PlayerActionEvent caller, float state)
+        {
+            if (state >= 1f || state <= -1f)
+            {
+                SoundByte.PlayOneShotGame("cocoaBar/whiff_temp");
+                return;
+            }
+            SoundByte.PlayOneShotGame("cocoaBar/whoosh_temp");
+            SoundByte.PlayOneShotGame("cocoaBar/sparkle_temp");
+        }
+
+        public void ServeMiss(PlayerActionEvent caller)
+        {
+            SoundByte.PlayOneShotGame("cocoaBar/whiff_temp");
+        }
+
+        public void Nothing(PlayerActionEvent caller)
+        {
+
+        }
+    }
+}
